3530f8e4f529c7c9afae097319e88f665b02d674
KizievDmitriy <92225148+KizievDmitriy@users.noreply.github.com>
2022-09-02 12:53:38 +0000

    Deploying to gh-pages from @ KizievDmitriy/minimo@97898577ca56a4a972a32efeb401ae37d9a9ecf1 🚀

---
+++ index.2f9b29fe.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".burger"),t=document.querySelector(".navrr"),o=document.querySelector("body");e.addEventListener("click",(function(){e.classList.toggle("toggled"),t.classList.toggle("show"),o.classList.toggle("overflow")}))}();
+//# sourceMappingURL=index.2f9b29fe.js.map
