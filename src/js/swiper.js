b7de20fc41afecb76d850acd8e0308096babfbf0
KizievDmitriy <kizievdmitrii@gmail.com>
2022-09-02 18:02:03 +0300

    add swiper hero

---
+++ src/js/swiper.js
@@ -0,0 +1,13 @@
+export  const swiper = new Swiper('.swiper', {
+          // Optional parameters
+          direction: 'horizontal',
+          loop: false,
+  
+          // Navigation arrows
+        
+          
+        //   autoplay: {
+        //   delay: 2000,
+        //   disableOnInteraction: false,
+        //   },
+       });
