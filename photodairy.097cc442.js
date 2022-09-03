5a6c795fa083095d11de2edd7c76ec51e939c541
KizievDmitriy <92225148+KizievDmitriy@users.noreply.github.com>
2022-09-03 19:53:37 +0000

    Deploying to gh-pages from @ KizievDmitriy/minimo@6b40bc60ac6254f95e7b695765329baed5b16c6e 🚀

--- photodairy.097cc442.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".burger"),o=document.querySelector(".navrr"),t=document.querySelector("body");e.addEventListener("click",(function(){e.classList.toggle("toggled"),o.classList.toggle("show"),t.classList.toggle("overflow")}));new Swiper(".swiper",{direction:"horizontal",loop:!1});
-//# sourceMappingURL=photodairy.097cc442.js.map
--- photodairy.97f9feef.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".burger"),o=document.querySelector(".navrr"),t=document.querySelector("body");e.addEventListener("click",(function(){e.classList.toggle("toggled"),o.classList.toggle("show"),t.classList.toggle("overflow")}));new Swiper(".swiper",{direction:"horizontal",loop:!1})}();
-//# sourceMappingURL=photodairy.97f9feef.js.map
